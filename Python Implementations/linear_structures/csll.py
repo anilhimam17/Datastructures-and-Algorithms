734d813e8cd89f7bc113da8ba3821c4f0a787031
anilhimam17 <anilhimamg@gmail.com>
2025-06-28 19:13:40 +0100

    feat(python): completed all the insertion methods for CSLL

--- Python Implementations/linear_structures/csll.py
+++ Python Implementations/linear_structures/csll.py
@@ -10,7 +10,7 @@ class Node:
         self.next: Node | None = None
 
     def __str__(self):
-        return f"Node(value={self.value}, next={self.next})"
+        return f"Node(value={self.value})"
 
 class CSLL:
     """Class that implements all the behaviour of the Circular Singly-Linked List."""
@@ -44,6 +44,23 @@ class CSLL:
                 cursor = cursor.next
 
         return list_str
+    
+    def __iter__(self):
+        """Provides an iterator for the CSLL."""
+        if not self._head:
+            raise IndexError("The list is current empty.")
+        else:
+            cursor: Node = self._head
+            while cursor:
+                yield cursor
+
+                # Exit Condition
+                if cursor == self._tail:
+                    break
+
+                # Update the loop control variable
+                assert cursor.next is not None, "Cursor cannot be none during traversal."
+                cursor = cursor.next
 
     # ==== Helper Properties to update the pointers ====
     @property
@@ -90,6 +107,7 @@ class CSLL:
             # Adds the element at the end of the list.
             elif index == self.no_of_elements - 1:
                 self.append(value)
+                return
             # Adds the element at any random index of the list.
             else:
                 cursor = self._head
@@ -106,14 +124,30 @@ class CSLL:
             # Updating the no of elements
             self.no_of_elements += 1
 
-    def search():
-        raise NotImplementedError
+    def search(self, value: int):
+        """Returns a integer after searching for a given value in the list."""
+        for idx, node in enumerate(self):
+            if node.value == value:
+                return idx
+        return -1
     
-    def get():
-        raise NotImplementedError
+    def get(self, index: int):
+        """Returns the node at a given index of the list."""
+        if index < 0 or index >= self.no_of_elements:
+            raise IndexError("The requested index is out of bounds for the given list.")
+        else:
+            for idx, node in enumerate(self):
+                if idx == index:
+                    return node
     
-    def set():
-        raise NotImplementedError
+    def set(self, value: int, index: int):
+        """Updates the value of given index of the list."""
+        if index < 0 or index >= self.no_of_elements:
+            raise IndexError("The requested index is out of bounds for the given list.")
+        else:
+            update_node = self.get(index)
+            assert update_node is not None, "The get function cannot return a None."
+            update_node.value = value
     
     def pop():
         raise NotImplementedError
--- Python Implementations/main_csll.py
+++ Python Implementations/main_csll.py
@@ -23,6 +23,15 @@ def main():
     first_csll.insert(-1000, 4)
     print(first_csll)
 
+    print("Searching for -10: ", first_csll.search(-1))
+    print("Searching for 10: ", first_csll.search(10))
+    print("Searching for 50: ", first_csll.search(50))
+    print("Searching for -1000: ", first_csll.search(-1000))
+
+    print("\nTraversing the list using the get()")
+    for i in range(first_csll.no_of_elements):
+        print(f"Element: {i} -> ", first_csll.get(i))
+
 # ==== Driver Code ====
 if __name__ == "__main__":
     main()
