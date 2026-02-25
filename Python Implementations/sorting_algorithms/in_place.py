27cd4a78cd986f398cb6dcc0afaf143da45cb05a
Purple Lightning <anilhimamg@gmail.com>
2026-02-25 22:54:23 +0000

    feat: insertion_sort works and refactored all algo for intelligent break on code structure efficiency with operator

--- Python Implementations/sorting_algorithms/in_place.py
+++ Python Implementations/sorting_algorithms/in_place.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+import operator
 
 
 class InPlace:
@@ -14,6 +15,8 @@ class InPlace:
             return InPlace.bubble_sort
         elif method_name == "selection_sort":
             return InPlace.selection_sort
+        elif method_name == "insertion_sort":
+            return InPlace.insertion_sort
         else:
             raise ValueError("The provided method name was invalid")
 
@@ -23,6 +26,10 @@ class InPlace:
 
         if order not in ["ascending", "descending"]:
             raise ValueError("The order string provided was invalid")
+        elif order == "ascending":
+            comparator = operator.gt
+        else:
+            comparator = operator.lt
 
         list_size = len(input_list)
 
@@ -31,13 +38,17 @@ class InPlace:
             # Inner Loop to apply pair-wise comparisions with bubbling limit
             # - i applied to not disturb already bubbled elements (Bubbling Limit)
             # - 1 applied to avert out of bound error during last element comparison
-            for j in range(list_size - i - 1):
-                if order == "ascending" and input_list[j] > input_list[j + 1]:
+
+            # Early stopping flag
+            swapped = False
+            for j in range(list_size - i - 1):    
+                if comparator(input_list[j], input_list[j + 1]):
                     input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
-                elif order == "descending" and input_list[j] < input_list[j + 1]:
-                    input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
-                else: 
-                    continue
+                    swapped = True
+                
+            # If no swaps have taken place list is sorted
+            if not swapped:
+                break
         
         return input_list
     
@@ -47,6 +58,10 @@ class InPlace:
 
         if order not in ["ascending", "descending"]:
             raise ValueError("The order string provided was invalid")
+        elif order == "ascending":
+            comparator = operator.lt
+        else:
+            comparator = operator.gt
 
         list_size = len(input_list)
 
@@ -59,15 +74,37 @@ class InPlace:
             # Start from the ith index to not disturb the already sorted part
             # + 1 applied to search all but the initial element as potential swap cases
             for j in range(i + 1, list_size):
-                if order == "ascending" and input_list[j] < input_list[swap_index]:
+                if comparator(input_list[j], input_list[swap_index]):
                     swap_index = j
-                elif order == "descending" and input_list[j] > input_list[swap_index]:
-                    swap_index = j
-                else:
-                    continue
-            
+
             # Swapping the selected element for the current iteration
             input_list[i], input_list[swap_index] = input_list[swap_index], input_list[i]
+
+        return input_list
+    
+    @staticmethod
+    def insertion_sort(input_list: list, order: str = "ascending") -> list:
+        """Performs insertion sort on the input list and returns the sorted list."""
+
+        if order not in ["ascending", "descending"]:
+            raise ValueError("The order string provided was invalid")
+        elif order == "ascending":
+            comparator = operator.lt
+        else:
+            comparator = operator.gt
+        
+        list_size = len(input_list)
+
+        # Outer Loop to iterate through the unsorted list for insertion into the sorted part
+        for i in range(1, list_size):
+            # Inner Loop to insert the element into the correct position
+            for j in range(i):
+                if comparator(input_list[i], input_list[j]):
+                    # Inserting the element at the correct location in sorted part
+                    input_list.insert(j, input_list[i])
+                    # Removing the element from the unsorted part
+                    input_list.pop(i + 1)
+                    break
 
         return input_list
 
