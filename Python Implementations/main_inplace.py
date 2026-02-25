f5db04003d3e859c6c8a2048de9b4216fa6a4ed2
Purple Lightning <anilhimamg@gmail.com>
2026-02-25 11:45:11 +0000

    refactor(main_inplace): generalised main for testing the performance of all the inplace algorithms

--- Python Implementations/main_inplace.py
+++ Python Implementations/main_inplace.py
@@ -26,22 +26,37 @@ def main() -> None:
         sort_algo = sort_handle.resolve_sorting_method(method_name=algo_name)
         print(f"\nApplying {algo_name}")
 
-        # Applying the Sorting Algorithm
+        # Applying the Sorting Algorithm for Ascending Order
         st_1 = time.time()
         ascending_list = sort_algo(input_list, "ascending")
-        print(f"\nIn Ascending Order {algo_name}:")
-        print(ascending_list[:50])
         et_1 = time.time()
 
+        # Checking the sorted list
+        if not all(ascending_list[i] < ascending_list[i + 1] for i in range(len(ascending_list) - 1)):
+            raise ValueError(f"Ascending List for {algo_name} wasn't sorted")
+        
+        # Viewing the first 50 elements
+        print(f"\nIn Ascending Order {algo_name}:")
+        print(ascending_list[:50])
+        
+        # Applying the Sorting Algorithm for Descending Order
         st_2 = time.time()
         descending_list = sort_algo(input_list, "descending")
+        et_2 = time.time()
+        
+        # Checking the sorted list
+        if not all(descending_list[i] > descending_list[i + 1] for i in range(len(descending_list) - 1)):
+            raise ValueError(f"Descending List for {algo_name} wasn't sorted")
+        
+        # Viewing the first 50 elements
         print(f"\nIn Descending Order {algo_name}:")
         print(descending_list[:50])
-        et_2 = time.time()
-
+        
+        # Calculating the average performance time for the algorithm
         avg_time = ((et_1 - st_1) + (et_2 - st_2)) / 2
         sort_performance[algo_name] = avg_time
-
+    
+    # Viewing the performance of the respective sorting algorithms
     print("\nAverage Sorting Times of the respective algorithms was:")
     for item in sort_performance.items():
         print(item)
