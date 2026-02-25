bd6c9c24205b32db3c9ef0f9c5db52cac5061ef2
Purple Lightning <anilhimamg@gmail.com>
2026-02-25 11:20:04 +0000

    feat: added method resolution to access sort_algo by str and selection_sort works

--- Python Implementations/sorting_algorithms/in_place.py
+++ Python Implementations/sorting_algorithms/in_place.py
@@ -1,6 +1,21 @@
+from collections.abc import Callable
+
+
 class InPlace:
     """This class implements the semantic structure 
     that house In-Place Sorting Algorithms."""
+
+    @staticmethod
+    def resolve_sorting_method(method_name: str) -> Callable[[list, str], list]:
+        """Retrieves the appropriate sorting algorithm based on the provide method_name 
+        and returns the callable to it."""
+
+        if method_name == "bubble_sort":
+            return InPlace.bubble_sort
+        elif method_name == "selection_sort":
+            return InPlace.selection_sort
+        else:
+            raise ValueError("The provided method name was invalid")
 
     @staticmethod
     def bubble_sort(input_list: list, order: str = "ascending") -> list:
@@ -21,5 +36,38 @@ class InPlace:
                     input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
                 elif order == "descending" and input_list[j] < input_list[j + 1]:
                     input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
+                else: 
+                    continue
         
         return input_list
+    
+    @staticmethod
+    def selection_sort(input_list: list, order: str = "ascending") -> list:
+        """Performs selection sort on the input list and returns the sorted list."""
+
+        if order not in ["ascending", "descending"]:
+            raise ValueError("The order string provided was invalid")
+
+        list_size = len(input_list)
+
+        # Outer Loop to orcheste the comparisons
+        for i in range(list_size):
+            
+            # Inner Loop to find the min / max elements for each iteration
+            swap_index = i
+            
+            # Start from the ith index to not disturb the already sorted part
+            # + 1 applied to search all but the initial element as potential swap cases
+            for j in range(i + 1, list_size):
+                if order == "ascending" and input_list[j] < input_list[swap_index]:
+                    swap_index = j
+                elif order == "descending" and input_list[j] > input_list[swap_index]:
+                    swap_index = j
+                else:
+                    continue
+            
+            # Swapping the selected element for the current iteration
+            input_list[i], input_list[swap_index] = input_list[swap_index], input_list[i]
+
+        return input_list
+
