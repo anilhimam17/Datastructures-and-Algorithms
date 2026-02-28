4ba806eae8cd94dafea50c1214980cb9f90cd2eb
Purple Lightning <anilhimamg@gmail.com>
2026-02-28 12:25:03 +0000

    feat: integrated implementation of quick sort and its helper

--- Python Implementations/sorting_algorithms/in_place.py
+++ Python Implementations/sorting_algorithms/in_place.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 import operator
+import random
 
 
 class InPlace:
@@ -17,6 +18,8 @@ class InPlace:
             return InPlace.selection_sort
         elif method_name == "insertion_sort":
             return InPlace.insertion_sort
+        elif method_name == "quick_sort":
+            return InPlace.quick_sort
         else:
             raise ValueError("The provided method name was invalid")
 
@@ -108,3 +111,85 @@ class InPlace:
 
         return input_list
 
+    @staticmethod
+    def quick_sort(input_list: list, order: str = "ascending") -> list:
+        """Performs the quick sort algorithm on the elements in place thereby returning 
+        the reference to the list."""
+
+        if order not in ["ascending", "descending"]:
+            raise ValueError("The Provide input for order type is invalid")
+        
+        input_list = InPlace._quick_sort_engine(
+            input_list=input_list,
+            low=0,
+            high=len(input_list) - 1,
+            order=order
+        )
+
+        return input_list
+        
+    @staticmethod
+    def _quick_sort_engine(input_list: list, low: int, high: int, order: str) -> list:
+        """Initiates the recursion call stack to partition and sort the list using quick sort."""
+        
+        # Recursive Case: If there exists a range between low and high continue parititioning and sort
+        if low < high:
+            # First Pivot Index
+            pivot_index = InPlace._quick_sort_helper(
+                input_list=input_list,
+                low=low,
+                high=high,
+                order=order
+            )
+
+            # Recursively Calling the Quick Sort helper for partitions around pivot index
+            # Sorting the Left Side Partition
+            InPlace._quick_sort_engine(
+                input_list=input_list,
+                low=low,
+                high=pivot_index - 1,
+                order=order
+            )
+            # Sorting the Right Side Partition
+            InPlace._quick_sort_engine(
+                input_list=input_list,
+                low=pivot_index + 1,
+                high=high,
+                order=order
+            )
+        
+        # Base Case: Return list if list size = 1 where low == high
+        return input_list
+
+    @staticmethod
+    def _quick_sort_helper(input_list: list, low: int, high: int, order: str) -> int:
+        """Performs the swapping of elements in the input list in accordance to the pivot element.
+        It then returns index location for the Pivot element for downstream sorts."""
+
+        if order == "ascending":
+            comparator = operator.le
+        else:
+            comparator = operator.ge
+        
+        # Calculating a randomised pivot element
+        pivot_idx = random.randint(low, high)
+        input_list[high], input_list[pivot_idx] = input_list[pivot_idx], input_list[high]
+        pivot_element = input_list[high]
+
+        # Boundary Index which will reduce to pivot index at the end of pair-wise swapping
+        boundary_index = low - 1
+
+        # Iterating through the partition to order the elements in accordance to Pivot using comparator
+        for i in range(low, high):
+            if comparator(input_list[i], pivot_element):
+                # Updating the new boundary
+                boundary_index += 1
+                input_list[boundary_index], input_list[i] = input_list[i], input_list[boundary_index]
+        
+        # Moving the pivot into order
+        input_list[boundary_index + 1], input_list[high] = (
+            input_list[high],
+            input_list[boundary_index + 1]
+        )
+
+        return boundary_index + 1
