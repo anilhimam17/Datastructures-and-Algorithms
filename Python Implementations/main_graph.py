729f2bbe6e6c4363612cc2df57abba610e98c66f
Purple Lightning <anilhimamg@gmail.com>
2026-03-02 11:59:08 +0000

    refactor: integrated the api changes to test the refacotred Graph class

--- Python Implementations/main_graph.py
+++ Python Implementations/main_graph.py
@@ -12,30 +12,38 @@ def main() -> None:
     for i in range(5):
         first_graph.add_vertex(vertex_name=chr(i + 65))
 
+    print("\nThe Initial Adjacency Matrix without any of the edges added:")
+    print(first_graph)
+
     # Adding Edges between the given Vertex Pairs
     edge_pairs = [
         ("A", "B"), ("B", "E"), ("E", "D"), 
-        ("D", "C"), ("C", "A"), ("D", "A")
+        ("D", "C"), ("C", "A"), ("D", "A"), ("A", "A")
     ]
     print("\nAdding edges between the Vertices in the Graph")
     for v1, v2 in edge_pairs:
-        first_graph.add_edge(vertex_1=v1, vertex_2=v2)
-
-    print(f"\nThe Adjacency Matrix of the Graph:\n{first_graph.adj_matrix}")
+        try:
+            first_graph.add_edge(vertex_1=v1, vertex_2=v2)
+        except ValueError:
+            print("Encountered a looped edge insertion and ignored")
+            continue
+    print(first_graph)
 
     # Removing Edge between the given Vertex Pairs
-    remove_pairs = [("A", "D")]
+    remove_pairs = [("A", "D"), ("A", "A")]
     print("\nRemoving edges between the Vertices in the Graph")
     for v1, v2 in remove_pairs:
-        first_graph.remove_edge(vertex_1=v1, vertex_2=v2)
-
-    print(f"\nThe Adjacency Matrix of the Graph:\n{first_graph.adj_matrix}")
+        try:
+            first_graph.remove_edge(vertex_1=v1, vertex_2=v2)
+        except ValueError:
+            print("Encounted a looped edge removal and ignored")
+            continue
+    print(first_graph)
 
     # Removing a Vertex from the Graph
-    print("Removing Vertex C from the Graph")
+    print("\nRemoving Vertex C from the Graph")
     first_graph.remove_vertex(vertex_name="C")
-
-    print(f"\nThe Adjacency Matrix of the Graph:\n{first_graph.adj_matrix}")
+    print(first_graph)
 
 
 if __name__ == "__main__":
