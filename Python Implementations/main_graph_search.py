1af77db3527fc061586b4b1e9164e34c399241cb
Purple Lightning <anilhimamg@gmail.com>
2026-03-04 22:07:04 +0000

    refactor: added test case for directed graph construction and SSSP search with Dijkstra's Algorithm

--- Python Implementations/main_graph_search.py
+++ Python Implementations/main_graph_search.py
@@ -1,6 +1,7 @@
 from linear_structures.stack import Stack
 from linear_structures.queue import Queue
 from non_linear_structures.graph import Graph
+from non_linear_structures.heap import Heap
 from searching_algorithms.graph_search import GraphSearch
 
 
@@ -25,13 +26,15 @@ def main() -> None:
     for vertex in simple_graph.vertex_map.keys():
         print(simple_graph.get_neighbours(vertex_name=vertex))
 
-    print("\nPeforming Depth First Search on the Graph from all the existing nodes:")
+    # Initialising the Graph Search Object
     graph_search = GraphSearch()
+    
+    # Depth First Search
+    print("\nPeforming Depth First Search on the Graph from all the existing nodes:")
     for node in simple_graph.vertex_map.keys():
         explored_vertices = []
         frontier = Stack()
         
-        # DFS
         graph_search.depth_first_search(
             graph=simple_graph,
             vertex_name=node,
@@ -44,12 +47,12 @@ def main() -> None:
     for vertex in simple_graph.vertex_map.keys():
         print(simple_graph.get_neighbours(vertex_name=vertex))
 
+    # Breadth First Search
     print("\nPeforming Breadth First Search on the Graph from all the existing nodes:")
     for node in simple_graph.vertex_map.keys():
         explored_vertices = []
         frontier = Queue()
         
-        # BFS
         graph_search.breadth_first_search(
             graph=simple_graph,
             vertex_name=node,
@@ -57,6 +60,47 @@ def main() -> None:
             frontier=frontier
         )
         print(f"BFS from {node} as start:\n{explored_vertices}")
+
+    # Creating a Directed Graph for the Single Source Shortest Path Problem
+    directed_graph = Graph(graph_type="directed")
+    print("\nInserting 8 Vertices into the Directed Graph")
+    for i in range(8):
+        directed_graph.add_vertex(vertex_name=chr(i + 65))
+    print(directed_graph)
+
+    # Inserting the Weighted Edges
+    edge_cost_pairs = [
+        ("A", "B", 6), ("A", "C", 10), ("A", "D", 9),
+        ("B", "D", 5), ("B", "E", 16), ("B", "F", 13),
+        ("C", "D", 6), ("C", "H", 5), ("C", "G", 21),
+        ("D", "F", 8),
+        ("E", "G", 10),
+        ("F", "E", 4), ("F", "G", 12),
+        ("H", "F", 2), ("H", "G", 14)
+    ]
+    print("\nInserting all the weighted edges into the Directed Graph")
+    for v1, v2, w in edge_cost_pairs:
+        directed_graph.add_edge(vertex_1=v1, vertex_2=v2, edge_weight=w)
+    print(directed_graph)
+
+    # Setting the Parameters for the SSSP
+    source_node = "A"
+    explored_vertices = []
+    frontier = Heap(heap_size=len(directed_graph.vertex_map) ** 2)
+    
+    # Dijkstra's Search
+    print(f"\nApplying Dijkstra's Algorithm to find the shortest path to all nodes from Source: {source_node}")
+    shortest_paths = graph_search.dijkstra_search(
+        graph=directed_graph,
+        source_vertex=source_node,
+        explored_vertices=explored_vertices,
+        frontier=frontier
+    )
+
+    # Resulting Shortest Paths
+    print("Shortest Paths:")
+    for n_name, (n_cost, n_pred) in shortest_paths:
+        print(f"\nNode: {n_name}\nPredecessor: {n_pred} | Cost: {n_cost}")
 
 
 if __name__ == "__main__":
